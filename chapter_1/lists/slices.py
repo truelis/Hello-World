eb0001fd47bc39d5a6c48f5e9f6f484406265090
Dimitris Troulis <dimitris.troulis@mvfglobal.com>
2024-05-21 00:00:01 +0100

    [DT] Added some slicing exercises

---
+++ chapter_1/lists/slices.py
@@ -0,0 +1,7 @@
+guest_list = ["Bill Gates", "Elon Musk", "Gio", "Vagias", "Spiros"]
+
+print(f"\nThe 3 first guests are: \n{guest_list[:3]}")
+
+print(f"\nThe 3  guests from the middle onwards are: \n{guest_list[2:5]}")
+
+print(f"\nThe last 3  guests are: \n{guest_list[-3:]}")
